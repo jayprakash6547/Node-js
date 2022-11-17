f0e27641bc153767154ed652b37e28df2d591d8b
jayprakash6547 <87560854+jayprakash6547@users.noreply.github.com>
2022-11-17 21:21:01 +0530

    init

--- 1.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-let text = fs.readFileSync("delete.txt","utf-8");
-text = text.replace("3rd year" ,"5th Sem");
-console.log("The content of the file is ")
-console.log(text);
-console.log("Creating a new file");
-fs.writeFileSync("jay.txt",text)
--- express/app.js
+++ express/app.js
@@ -1,10 +1,13 @@
+//It is used to import any module and in this module i am import express so i use express 
+
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 80;
-
+// For serving static files
 app.use("/static", express.static("static"));
 
+// set the template engine as pug
 app.set("view engine", "pug");
 
 //set the views directory
